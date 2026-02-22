d3373b52e166dc9235d871312501888c9a549e5c
Joseph Omondi <omondi@Josephs-MacBook-Pro.local>
2026-02-22 11:40:57 +0300

    5th commit: created models and registered them on the admin site

--- wastewater_pollution/core/admin.py
+++ wastewater_pollution/core/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.db import models
+
+# Create your models here.
+from django.contrib import admin
+from .models import RawArticle, BlogPost
+
+
+@admin.register(RawArticle)
+class RawArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'published_at', 'scraped_at']
+    search_fields = ['title', 'author']
+    readonly_fields = ['scraped_at']
+    list_filter = ['published_at']
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'generated_at']
+    search_fields = ['title']
+    list_filter = ['status', 'generated_at']
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ['generated_at']
+
+
+
--- wastewater_pollution/core/models.py
+++ wastewater_pollution/core/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+from django.db import models 
+from django.utils.text import slugify
 
-# Create your models here.
+
+class RawArticle(models.Model):
+    title = models.TextField() 
+    url = models.URLField(unique=True) 
+    content_raw = models.TextField() 
+    author=models.TextField(max_length=50) 
+    hash = models.CharField(max_length=64, unique=True)
+    published_at = models.DateTimeField(null=True)
+    scraped_at = models.DateTimeField(auto_now_add=True)
+
+
+class BlogPost(models.Model): 
+    raw_article = models.ForeignKey(RawArticle, on_delete=models.CASCADE) 
+    title = models.TextField() 
+    content_html = models.TextField() 
+    status = models.CharField(max_length=20, default="draft") 
+    slug=models.SlugField(max_length=300, unique=True) 
+    generated_at = models.DateTimeField(auto_now_add=True)
+    raw_article = models.OneToOneField(RawArticle, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+
+            while BlogPost.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
